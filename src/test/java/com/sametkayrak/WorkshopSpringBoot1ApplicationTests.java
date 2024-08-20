cc952048a4c808e403154de59089bf3db077dbbc
Samet Kayrak <abdus.kayrak@gmail.com>
2024-08-20 10:06:01 +0300

    Initial commit

---
+++ src/test/java/com/sametkayrak/WorkshopSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametkayrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBoot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
